c7b8c9ad53dbf540e0b31e34e442e4ec8784421b
Anna <annatran93@gmail.com>
2023-10-26 14:17:57 -0400

    replace jquery with vanilla js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-$(document).ready(function () {
-    $('#registration-form').on('submit', function (event) {
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#registration-form').addEventListener('submit', function (event) {
 
         event.preventDefault();
 
-        const name = $('#name').val();
-        const email = $('#email').val();
+        const name = document.querySelector('#name').value;
+        const email = document.querySelector('#email').value;
         const emailDomain = email.substr(email.indexOf("@") + 1);
         const emailService = emailDomain.substr(0, emailDomain.indexOf('.'));
-        const checkbox = $('#checkbox').is(':checked');
+        const checkbox = document.querySelector('#checkbox').checked;
 
         const emailFormat = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
@@ -17,20 +17,20 @@ $(document).ready(function () {
             if (checkbox === false) {
                 alert('You must agree to the terms and conditions')
             } else {
-                $('.registree-name').text(name);
-                $('.email-address').text(emailService);
-                $('#modal-overlay').css('display', 'flex');
+                document.querySelector('.registree-name').innerText = name;
+                document.querySelector('.email-address').innerText = emailService;
+                document.querySelector('#modal-overlay').style.display = 'flex';
             }
         }
     });
 
-    $('#close-button').on('click', function (event) {
-        $('#modal-overlay').css('display', 'none');
+    document.querySelector('#close-button').addEventListener('click', function (event) {
+        document.querySelector('#modal-overlay').style.display = 'none';
     })
 
-    $('#modal-overlay').on('click', function (event) {
-        if ($(event.target).hasClass('modal-overlay')) {
-            $('#modal-overlay').css('display', 'none');
+    document.querySelector('#modal-overlay').addEventListener('click', function (event) {
+        if ((event.target).classList.contains('modal-overlay')) {
+            document.querySelector('#modal-overlay').style.display = 'none';
         }
     })
 });
